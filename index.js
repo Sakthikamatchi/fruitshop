3d1756ea6823ae43dc7b7cb7caca676f088f3226
Sakthi <126490676+Sakthikamatchi@users.noreply.github.com>
2023-08-08 09:52:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+var buybutton1 = document.getElementById("buybutton1");
+var buybutton2 = document.getElementById("buybutton2");
+var buybutton3 = document.getElementById("buybutton3");
+
+
+buybutton1.addEventListener("click", function() {
+    
+    alert("Banana purchased! Thank you for your purchase.");
+  
+});
+buybutton2.addEventListener("click", function() {
+    
+    alert("Avacados purchased! Thank you for your purchase.");
+  
+});
+buybutton3.addEventListener("click", function() {
+    
+    alert("Jackfruit purchased! Thank you for your purchase.");
+  
+});
+
---
+++ pass.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var registerButton = document.getElementById("registerButton");
+  registerButton.addEventListener("click", registerUser);
+
+  function registerUser() {
+      var username = document.getElementById("username").value;
+      var email = document.getElementById("email").value;
+      var password = document.getElementById("password").value;
+      var confirmPassword = document.getElementById("confirmPassword").value;
+
+      var passwordCriteria = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+,-./:;<=>?@[\\]^_`{|}~]).{8,}$/;
+      if (!password.match(passwordCriteria)) {
+          alert("Password must contain at least 8 characters, including at least one uppercase letter, one lowercase letter, one digit, and one special character.");
+          return;
+      }
+
+      if (password !== confirmPassword) {
+          alert("Passwords do not match. Please try again.");
+          return;
+      }
+
+      alert("Registration successful!\nUsername: " + username + "\nEmail: " + email);
+  }
+});
+
